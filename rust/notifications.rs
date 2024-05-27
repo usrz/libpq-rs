61a9a01d91dc1d8a2ccd486a20d54070b0286c2c
Pier Fumagalli <pier@usrz.com>
2024-05-27 10:39:07 +0200

    Notifications implementation

---
+++ rust/notifications.rs
@@ -0,0 +1,21 @@
+use crate::errors::PQError;
+use crate::ffi;
+use crate::errors::PQResult;
+
+pub struct PQNotification {
+  pub channel: String,
+  pub be_pid: i32,
+  pub extra: Option<String>,
+}
+
+impl TryFrom<*mut pq_sys::pgNotify> for PQNotification {
+  type Error = PQError;
+
+  fn try_from(value: *mut pq_sys::pgNotify) -> PQResult<Self> {
+    let foo: &pq_sys::pgNotify = unsafe { &*{value} };
+    let channel = ffi::to_string(foo.relname)?;
+    let extra = ffi::to_string_lossy(foo.extra);
+    let be_pid = foo.be_pid;
+    Ok(Self { channel, be_pid, extra })
+  }
+}
