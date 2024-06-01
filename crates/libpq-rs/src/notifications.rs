dbc509bc06a6290c1d27dd3f2592bed7f5e85f52
Pier Fumagalli <pier@usrz.com>
2024-06-01 23:39:26 +0200

    Cleanup Neon code

--- crates/libpq-rs/src/notifications.rs
+++ crates/libpq-rs/src/notifications.rs
@@ -2,7 +2,6 @@
 
 use crate::errors::*;
 use crate::ffi::*;
-// use neon::prelude::*;
 use std::fmt::Debug;
 
 #[derive(Debug)]
@@ -28,28 +27,4 @@ impl TryFrom<*mut pq_sys::pgNotify> for PQNotification {
 
     Ok(Self { channel, be_pid, extra })
   }
-}
-
-impl PQNotification {
-  // / Converts a [`PQNotification`] struct into a JavaScript object.
-  // /
-  // pub fn to_js_object<'a, C: Context<'a>>(
-  //   &self,
-  //   cx: &mut C,
-  // ) -> NeonResult<Handle<'a, JsObject>> {
-  //   let object = cx.empty_object();
-
-  //   let channel = cx.string(&self.channel);
-  //   object.set(cx, "channel", channel)?;
-
-  //   let be_pid = cx.number(self.be_pid);
-  //   object.set(cx, "be_pid", be_pid)?;
-
-  //   if let Some(extra) = &self.extra {
-  //     let extra = cx.string(extra);
-  //     object.set(cx, "extra", extra)?;
-  //   }
-
-  //   Ok(object)
-  // }
 }
