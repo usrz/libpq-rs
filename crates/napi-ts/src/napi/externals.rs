c22d855cf59940890d97dea115998e802c5990a4
Pier Fumagalli <pier@usrz.com>
2024-06-07 20:57:22 +0200

    Cleanup and generics (with finalizable)

--- crates/napi-ts/src/napi/externals.rs
+++ crates/napi-ts/src/napi/externals.rs
@@ -1,40 +1,18 @@
 use super::*;
 
 use nodejs_sys::*;
-use std::marker::PhantomData;
 use std::mem::MaybeUninit;
 use std::mem;
 use std::os::raw;
-use std::ptr;
 use std::any::Any;
-
-// ========================================================================== //
-// INTERNAL WRAPPING                                                          //
-// ========================================================================== //
-
-struct FinalizerWrapper<T> {
-  phantom: PhantomData<T>
-}
-
-impl <T> FinalizerWrapper<T> {
-  fn new() -> Self {
-    Self { phantom: PhantomData }
-  }
-
-  fn finalize(self, data: *mut raw::c_void) {
-    println!("Dropping {} @ {:?}", std::any::type_name::<T>(), data);
-    let boxed = unsafe { Box::from_raw(data as *mut T) };
-    drop(boxed);
-  }
-}
+use std::ptr;
 
 // ========================================================================== //
 // TRAMPOLINE                                                                 //
 // ========================================================================== //
 
-extern "C" fn finalizer_trampoline<T>(_: napi_env, data: *mut raw::c_void, hint: *mut raw::c_void) {
-  let finalizer = unsafe { Box::from_raw(hint as *mut FinalizerWrapper<T>) };
-  finalizer.finalize(data);
+extern "C" fn finalizer_trampoline<T: Finalizable>(_: napi_env, data: *mut raw::c_void, _: *mut raw::c_void) {
+  unsafe { Box::from_raw(data as *mut T) }.finalize();
 }
 
 type FinalizerTrampoline =
@@ -48,11 +26,11 @@ type FinalizerTrampoline =
 // PUBLIC FACING                                                              //
 // ========================================================================== //
 
-pub fn add_finalizer<T>(value: Value, pointer: *mut T) {
+pub fn add_finalizer<T: Finalizable>(value: Value, data: *mut T) {
   unsafe {
-    // Build a wrapper for our trampoline and leak it as well
-    let finalizer = FinalizerWrapper::<T>::new();
-    let finalizer = Box::into_raw(Box::new(finalizer));
+    // Box the data, and leak the raw pointer
+    let boxed = Box::new(data);
+    let pointer = Box::into_raw(boxed);
 
     // Get a hold on our trampoline's pointer (and erase its type!)
     let trampoline = finalizer_trampoline::<T>;
@@ -63,17 +41,17 @@ pub fn add_finalizer<T>(value: Value, pointer: *mut T) {
       value,
       pointer as *mut raw::c_void,
       Some(trampoline),
-      finalizer as *mut raw::c_void,
-      ptr::null_mut()
+      ptr::null_mut(), // no hint
+      ptr::null_mut() // no need for a napi_reference
     );
   }
 }
 
-pub fn create_value_external<T>(data: *mut T) -> Value {
+pub fn create_value_external<T: Finalizable>(data: T) -> Value {
   unsafe {
-    // Build a wrapper for our trampoline and leak it as well
-    let finalizer = FinalizerWrapper::<T>::new();
-    let finalizer = Box::into_raw(Box::new(finalizer));
+    // Box the data, and leak the raw pointer
+    let boxed = Box::new(data);
+    let pointer = Box::into_raw(boxed);
 
     // Get a hold on our trampoline's pointer (and erase its type!)
     let trampoline = finalizer_trampoline::<T>;
@@ -82,14 +60,15 @@ pub fn create_value_external<T>(data: *mut T) -> Value {
     // Handle for our external data
     let mut result = MaybeUninit::<Value>::zeroed();
 
-    // Create the
+    // Create the external
     napi_check!(
       napi_create_external,
-      data as *mut raw::c_void,
+      pointer as *mut raw::c_void,
       Some(trampoline),
-      finalizer as *mut raw::c_void,
+      ptr::null_mut(), // no hint
       result.as_mut_ptr()
     );
+
     result.assume_init()
   }
 }
