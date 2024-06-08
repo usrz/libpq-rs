3362bf891a7cbd378917beb1432a9dada173339b
Pier Fumagalli <pier@usrz.com>
2024-06-09 00:42:37 +0200

    Lazy "bigint"

--- crates/napi-ts/src/types/bigint.rs
+++ crates/napi-ts/src/types/bigint.rs
@@ -1,20 +1,26 @@
 use crate::napi;
 use crate::types::*;
+use std::cell::Cell;
 
 #[derive(Clone, Debug)]
 pub struct NapiBigint {
-  value: i128,
+  value: Cell<Option<i128>>,
+  handle: Option<napi::Handle>,
 }
 
 impl NapiShape for NapiBigint {}
 
 impl NapiShapeInternal for NapiBigint {
   fn into_napi_value(self) -> napi::Handle {
-    napi::create_bigint_words(self.value)
+    if let Some(handle) = self.handle {
+      return handle
+    } else {
+      napi::create_bigint_words(self.value.get().unwrap())
+    }
   }
 
-  fn from_napi_value(value: napi::Handle) -> Self {
-    Self { value: napi::get_value_bigint_words(value) }
+  fn from_napi_value(handle: napi::Handle) -> Self {
+    Self { value: Cell::new(None), handle: Some(handle) }
   }
 }
 
@@ -22,13 +28,7 @@ impl NapiShapeInternal for NapiBigint {
 
 impl From<i32> for NapiBigint {
   fn from(value: i32) -> Self {
-    Self { value: value as i128 }
-  }
-}
-
-impl Into<i32> for NapiBigint {
-  fn into(self) -> i32 {
-    self.value as i32
+    Self::from(value as i128)
   }
 }
 
@@ -36,13 +36,7 @@ impl Into<i32> for NapiBigint {
 
 impl From<u32> for NapiBigint {
   fn from(value: u32) -> Self {
-    Self { value: value as i128 }
-  }
-}
-
-impl Into<u32> for NapiBigint {
-  fn into(self) -> u32 {
-    self.value as u32
+    Self::from(value as i128)
   }
 }
 
@@ -50,13 +44,7 @@ impl Into<u32> for NapiBigint {
 
 impl From<i64> for NapiBigint {
   fn from(value: i64) -> Self {
-    Self { value: value as i128 }
-  }
-}
-
-impl Into<i64> for NapiBigint {
-  fn into(self) -> i64 {
-    self.value as i64
+    Self::from(value as i128)
   }
 }
 
@@ -64,13 +52,7 @@ impl Into<i64> for NapiBigint {
 
 impl From<u64> for NapiBigint {
   fn from(value: u64) -> Self {
-    Self { value: value as i128 }
-  }
-}
-
-impl Into<u64> for NapiBigint {
-  fn into(self) -> u64 {
-    self.value as u64
+    Self::from(value as i128)
   }
 }
 
@@ -78,20 +60,31 @@ impl Into<u64> for NapiBigint {
 
 impl From<i128> for NapiBigint {
   fn from(value: i128) -> Self {
-    Self { value }
+    Self { value: Cell::new(Some(value)), handle: None }
   }
 }
 
 impl Into<i128> for NapiBigint {
   fn into(self) -> i128 {
-    self.value
+    match self.value.into_inner() {
+      None => napi::get_value_bigint_words(self.handle.unwrap()),
+      Some(value) => value,
+    }
   }
 }
 
 // ===== EXTRA METHODS =========================================================
 
 impl NapiBigint {
+  pub fn new(value: i128) -> Self {
+    Self::from(value)
+  }
+
   pub fn value(&self) -> i128 {
-    self.value
+    self.value.get().unwrap_or_else(|| {
+      let value = napi::get_value_bigint_words(self.handle.unwrap());
+      self.value.set(Some(value));
+      value
+    })
   }
 }
