1567a34789e85d760cbf2f499358a2fbdb35986f
Pier Fumagalli <pier@usrz.com>
2024-06-09 00:14:44 +0200

    Lazy initialization of strings

--- crates/napi-ts/src/types/string.rs
+++ crates/napi-ts/src/types/string.rs
@@ -1,53 +1,57 @@
 use crate::napi;
 use crate::types::*;
+use std::cell::RefCell;
 
 #[derive(Clone, Debug)]
 pub struct NapiString {
-  value: String,
+  value: RefCell<Option<String>>,
+  handle: Option<napi::Value>,
 }
 
 impl NapiShape for NapiString {}
 
 impl NapiShapeInternal for NapiString {
   fn into_napi_value(self) -> napi::Value {
-    napi::create_string_utf8(&self.value)
+    if let Some(handle) = self.handle {
+      return handle
+    } else {
+      let string = self.value.into_inner().unwrap();
+      napi::create_string_utf8(string.as_str())
+    }
   }
 
-  fn from_napi_value(value: napi::Value) -> Self {
-    Self { value: napi::get_value_string_utf8(value) }
+  fn from_napi_value(handle: napi::Value) -> Self {
+    Self { value: RefCell::new(None), handle: Some(handle) }
   }
 }
 
 // ===== &STR CONVERSION =======================================================
 
-impl From<&str> for NapiString {
-  fn from(value: &str) -> Self {
-    Self { value: value.to_string() }
+impl <S: AsRef<str>> From<S> for NapiString {
+  fn from(value: S) -> Self {
+    let string = value.as_ref().to_string();
+    Self { value: RefCell::new(Some(string)), handle: None }
   }
 }
 
 // ===== STRING CONVERSION =====================================================
 
-impl From<String> for NapiString {
-  fn from(value: String) -> Self {
-    Self { value }
-  }
-}
-
 impl Into<String> for NapiString {
   fn into(self) -> String {
-    self.value
+    self.value()
   }
 }
 
 // ===== EXTRA METHODS =========================================================
 
 impl NapiString {
-  pub fn new(string: &str) -> Self {
-    Self { value: string.to_string() }
+  pub fn new<S: AsRef<str>>(string: S) -> Self {
+    Self::from(string.as_ref())
   }
 
   pub fn value(&self) -> String {
-    self.value.clone()
+    self.value.borrow_mut().get_or_insert_with(|| {
+      napi::get_value_string_utf8(self.handle.unwrap())
+    }).clone()
   }
 }
