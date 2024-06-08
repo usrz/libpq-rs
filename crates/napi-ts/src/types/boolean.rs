1c21d52c3bbf6a9e0067478f6b18a55d147adb1b
Pier Fumagalli <pier@usrz.com>
2024-06-09 00:46:43 +0200

    Lazy bool

--- crates/napi-ts/src/types/boolean.rs
+++ crates/napi-ts/src/types/boolean.rs
@@ -1,20 +1,26 @@
 use crate::napi;
 use crate::types::*;
+use std::cell::Cell;
 
 #[derive(Clone, Debug)]
 pub struct NapiBoolean {
-  value: bool,
+  value: Cell<Option<bool>>,
+  handle: Option<napi::Handle>,
 }
 
 impl NapiShape for NapiBoolean {}
 
 impl NapiShapeInternal for NapiBoolean {
   fn into_napi_value(self) -> napi::Handle {
-    napi::get_boolean(self.value)
+    if let Some(handle) = self.handle {
+      return handle
+    } else {
+      napi::get_boolean(self.value.get().unwrap())
+    }
   }
 
-  fn from_napi_value(value: napi::Handle) -> Self {
-    Self { value: napi::get_value_bool(value) }
+  fn from_napi_value(handle: napi::Handle) -> Self {
+    Self { value: Cell::new(None), handle: Some(handle) }
   }
 }
 
@@ -22,13 +28,16 @@ impl NapiShapeInternal for NapiBoolean {
 
 impl From<bool> for NapiBoolean {
   fn from(value: bool) -> Self {
-    Self { value }
+    Self { value: Cell::new(Some(value)), handle: None }
   }
 }
 
 impl Into<bool> for NapiBoolean {
   fn into(self) -> bool {
-    self.value
+    match self.value.into_inner() {
+      None => napi::get_value_bool(self.handle.unwrap()),
+      Some(value) => value,
+    }
   }
 }
 
@@ -36,6 +45,10 @@ impl Into<bool> for NapiBoolean {
 
 impl NapiBoolean {
   pub fn value(&self) -> bool {
-    self.value
+    self.value.get().unwrap_or_else(|| {
+      let value = napi::get_value_bool(self.handle.unwrap());
+      self.value.set(Some(value));
+      value
+    })
   }
 }
