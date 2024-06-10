b6bb9eddb10151d282b383e3e3da384ffe97aebd
Pier Fumagalli <pier@usrz.com>
2024-06-10 20:00:43 +0200

    Reorganize types

--- crates/napi-ts/src/types/mod.rs
+++ crates/napi-ts/src/types/mod.rs
@@ -3,6 +3,7 @@ use std::fmt::Debug;
 
 mod bigint;
 mod boolean;
+mod external;
 mod null;
 mod number;
 mod object;
@@ -14,6 +15,7 @@ mod value;
 
 pub use bigint::*;
 pub use boolean::*;
+pub use external::*;
 pub use null::*;
 pub use number::*;
 pub use object::*;
@@ -22,6 +24,10 @@ pub use string::*;
 pub use symbol::*;
 pub use undefined::*;
 pub use value::*;
+use std::marker::PhantomData;
+use crate::NapiErr;
+
+// ===== CONVERSION ============================================================
 
 pub(crate) trait NapiFrom<T>: Sized {
   fn napi_from(value: T, env: napi::Env) -> Self;
@@ -40,9 +46,37 @@ where
   }
 }
 
-pub (crate) trait NapiTypeInternal<'a> {
-  fn from_napi(env: napi::Env, handle: napi::Handle) -> Self;
-  fn napi_handle(&self) -> napi::Handle;
+// ===== HANDLES ===============================================================
+
+pub struct NapiHandle<'a> {
+  pub (crate) phantom: PhantomData<&'a mut ()>,
+  pub (crate) handle: napi::Handle,
+  pub (crate) env: napi::Env,
+}
+
+impl <'a> NapiHandle<'a> {
+  pub (crate) fn from_napi(env: napi::Env, handle: napi::Handle) -> Self {
+    Self { phantom: PhantomData, handle, env }
+  }
+}
+
+// ===== TYPES =================================================================
+
+pub (crate) trait NapiTypeInternal<'a>: Sized {
+  fn get_napi_handle(&self) -> &NapiHandle<'a>;
+
+  fn from_napi_handle(handle: NapiHandle<'a>) -> Result<Self, NapiErr>;
+
+  fn from_napi_handle_unchecked(handle: NapiHandle<'a>) -> Self {
+    Self::from_napi_handle(handle).unwrap()
+  }
+
+  fn into_napi_handle(self) -> NapiHandle<'a> {
+    let handle = self.get_napi_handle();
+    let env = handle.env;
+    let handle = handle.handle;
+    NapiHandle { phantom: PhantomData, env, handle }
+  }
 }
 
 #[allow(private_bounds)]
