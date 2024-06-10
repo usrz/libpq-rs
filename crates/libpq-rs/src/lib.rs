d9b57d3c762119468e19614e05e9666e1ef67c65
Pier Fumagalli <pier@usrz.com>
2024-06-10 20:06:17 +0200

    More testing

--- crates/libpq-rs/src/lib.rs
+++ crates/libpq-rs/src/lib.rs
@@ -6,6 +6,7 @@ use ffi::to_string_lossy;
 use conninfo::PQConninfo;
 use context::NapiContext;
 use context::MainContext;
+use context::NapiReturn;
 pub mod connection;
 pub mod conninfo;
 pub mod debug;
@@ -56,57 +57,33 @@ struct Foobar {
 
 unsafe impl Send for Foobar { }
 
-napi_ts::napi_init!(|mut env, exports| {
+
+
+napi_ts::napi_init!(|env, exports| {
   println!("Initializing...");
   println!("  openssl version: {}", openssl_version());
   println!("    libpq version: {} (threadsafe={})", libpq_version(), libpq_threadsafe());
 
-  // let info = Foobar { s: "foobar BAAZ".to_string() };
-  // let external = NapiExternal::new(info);
-  // println!("EXTERNAL IS {:?}", external);
-
-  // let _f = NapiFunction::named("my great function", move |_, args| {
-  //   args[2].downcast::<NapiFunction>()
-  //     .and_then(|value| value.call(&[ NapiNull::new() ]))?;
-
-  //   NapiReturn::void()
-  // });
-
-  // let _f2 = NapiFunction::named("another function", |_, args| {
-  //   // println!("REAL THE EXTERNAL IS {:?}", external);
-  //   println!("ARG FROM EXTERNAL IS {:?}", args[0]);
-  //   let qqqq = args[0].downcast::<NapiExternal<Foobar>>().unwrap();
-  //   println!("FROM ARGUMENT {:?}", qqqq);
-  //   println!("FROM ARGUMENT {:?}", qqqq.s);
-  //   // qqqq.iter().for_each(|(key, val)| {
-  //   //   println!("DEREFERENCED {} => {}", key, val);
-  //   // });
-
-  //   println!("SECOND CALLBACK!!!");
-  //   NapiReturn::void()
-  // });
+  let ext: NapiValue = env.external(Foobar { s: "Hello, string".to_owned() }).into();
+  let ext2: NapiExternal<Foobar> = ext.try_into()?;
 
 
-  // let _s1 = NapiSymbol::new("foobar");
-  // let _s2 = NapiSymbol::symbol_for("foobar");
-  // println!("S1 {:?}", _s1.description());
-  // println!("S2 {:?}", _s2.description());
+  let blurb = Blurb {};
 
-  exports
-  //   .set_property("foo", &_f)
-  //   .set_property("bar", &_f2)
-  //   .set_property("external", &external)
-    .set_property_string("openssl_version", openssl_version())
-    .set_property_string("libpq_version", libpq_version())
-    .set_property_boolean("libpq_threadsafe", libpq_threadsafe())
-    .set_property("baz",&env.object())
-  ;
+  blurb.call(move || {
+    println!("{:?} {:?}", ext2, "foo");
+  });
 
-  Ok(exports)
+  exports.ok()
 });
 
+struct Blurb {}
 
-// pub fn test(env: &mut MainContext) {
-//   env.bigint(123);
-//   // NapiBigint
-// }
+impl Blurb {
+  pub fn call<F>(&self, callback: F)
+  where
+    F: Fn() + 'static,
+  {
+    callback();
+  }
+}
--- crates/napi-ts/src/init.rs
+++ crates/napi-ts/src/init.rs
@@ -3,45 +3,54 @@ use crate::napi;
 use crate::types::*;
 
 use std::panic;
+use std::panic::AssertUnwindSafe;
 use crate::context::MainContext;
 
-pub fn register_module<'a, R: NapiType<'a> + Sized>(
+pub fn register_module(
   env: napi::Env,
   exports: napi::Handle,
-  init: fn(MainContext<'a>, NapiObject<'a>) -> NapiResult<R>
+  init: fn(MainContext, NapiObject) -> NapiResult
 ) -> napi::Handle {
+  println!("REGISTERING");
 
-  // Create a new "Napi" environment
+  let safe = AssertUnwindSafe(init);
 
   // Call up our initialization function with exports wrapped in a NapiObject
   // and unwrap the result into a simple "napi_value" (the pointer)
   let panic = panic::catch_unwind(|| {
-    let context = MainContext::new(env);
-    let exports = NapiObject::from_napi(env, exports);
-    init(context, exports)
-      .map(|ret| -> napi::Handle { ret.napi_handle() })
+    let handle = NapiHandle::from_napi(env, exports);
+
+    safe(MainContext::new(env), NapiObject::from_napi_handle(handle)?)
   });
 
   // See if the initialization panicked
   let result = panic.unwrap_or_else(|error| {
     if let Some(message) = error.downcast_ref::<&str>() {
-      Err(NapiError::from(format!("PANIC: {}", message)))
+      Err(format!("PANIC: {}", message).into())
     } else if let Some(message) = error.downcast_ref::<String>() {
-      Err(NapiError::from(format!("PANIC: {}", message)))
+      Err(format!("PANIC: {}", message).into())
     } else {
-      Err(NapiError::from("PANIC: Unknown error".to_owned()))
+      Err("PANIC: Unknown error".into())
     }
   });
 
   // When we get here, we dealt with possible panic situations, now we have
   // a result, which (if OK) will hold the napi_value to return to node or
   // (if ERR) will contain a NapiError to throw before returning
-  if let Err(error) = result {
-    let throwable = napi::create_error(env, error.to_string());
-    napi::throw(env, throwable); //TODO
+  match result {
+    Ok(exports) => exports.value,
+    Err(error) => {
+      napi::throw(env, error.value.unwrap_or_else(|| {
+        napi::create_error(env, error.to_string())
+      }));
+      // Just return the original exports unchanged... we'll throw anyhow!
+      exports
+    }
   }
 
-  // All done...
-  // drop(napi);
-  exports
+  // println!("REGISTERING");
+
+  // // All done...
+  // // drop(napi);
+  // exports
 }
